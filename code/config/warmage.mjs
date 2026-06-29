0a676acbc66e2f3ea6e8906fa4760be7ff80b71d
Jeff Hitchcock <mail@command-center.org>
2026-06-29 11:05:04 -0700

    Add class journals, register spell lists, adjust 2014 IDs

--- code/config/warmage.mjs
+++ code/config/warmage.mjs
@@ -1,4 +1,9 @@
 export function initialize() {
+	dnd5e.registry.spellLists.register(game.settings.get("dnd5e", "rulesVersion") === "legacy"
+		? "Compendium.mage-hand-press-core.journal.JournalEntry.mhpWarmage5e0000.JournalEntryPage.mhpWarmageSpellL"
+		: "Compendium.mage-hand-press-core.journal.JournalEntry.mhpWarmage55e000.JournalEntryPage.mhpWarmageSpellL"
+	);
+
 	if ( !game.settings.get("mage-hand-press-core", "warmage").customization ) return;
 	Object.assign(CONFIG.DND5E.featureTypes.class.subtypes, featureTypes.class);
 }
