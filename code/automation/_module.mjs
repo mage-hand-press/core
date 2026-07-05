203871fb3460ea38927253576721fb30822634b5
Jeff Hitchcock <mail@command-center.org>
2026-07-05 12:01:02 -0700

    Tweak Gunslinger features, deeds, and maneuvers

--- code/automation/_module.mjs
+++ code/automation/_module.mjs
@@ -1,3 +1,8 @@
+/* Gunslinger */
+
+// TODO: Mankiller (5e), add ability mod to firearm damage (only for attacks as part of action?)
+// TODO: Overkill (5.5e), add ability mod to damage for ranged attacks w/o mod, add 1d8 damage if it already has mod
+
 /* Warmage */
 export * as WarmageEdge from "./warmage-edge.mjs";
 
