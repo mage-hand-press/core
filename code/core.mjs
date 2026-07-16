1c92b717e1b8652702e9b2d769d8fe0eee318714
Jeff Hitchcock <mail@command-center.org>
2026-07-16 13:25:43 -0700

    Add equipment redirects to Gunslinger module if active

--- code/core.mjs
+++ code/core.mjs
@@ -3,6 +3,7 @@ import * as automation from "./automation/_module.mjs";
 import * as config from "./config/_module.mjs";
 import * as customization from "./customization/_module.mjs";
 import * as data from "./data/_module.mjs";
+import * as redirects from "./redirects.mjs";
 import * as settings from "./settings.mjs";
 import * as spellcasting from "./spellcasting/_module.mjs";
 
@@ -10,6 +11,7 @@ Hooks.once("init", () => {
 	const MODULE = game.modules.get("mage-hand-press-core");
 	Object.assign(MODULE, { applications, automation, customization, data, spellcasting });
 
+	redirects.initialize();
 	settings.initialize();
 
 	config.Gunslinger.initialize();
